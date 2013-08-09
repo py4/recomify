43a7500e9b4b4fe3135d4a0384f104245cc54d94
Celso Dantas <celsodantas@gmail.com>
2013-08-09 14:59:45 -0500

    removing unused code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,28 +55,3 @@ window.inputModal = function (prompt) {
     }
   });
 }
-
-// Make a delete form and submit it, non AJAX.
-window.deleteFormSubmit = function(location) {
-  var form = document.createElement('form');
-  form.setAttribute('method', 'POST');
-  form.setAttribute('action', location);
-  var h = document.createElement('input');
-  h.setAttribute('type', 'hidden');
-  h.setAttribute('name', '_method');
-  h.setAttribute('value', 'delete');
-  form.appendChild(h);
-  document.body.appendChild(form);
-  form.submit();
-}
-
-window.shopifyDeleteConfirm = function(location, message) {
-  if(!message) {
-    message = 'Are you sure you want to delete this?';
-  }
-  Shopify.API.Modal.confirm(message, function(result, data){
-    if(result){
-      deleteFormSubmit(location);
-    }
-  });
-}
