87038aa5baa5ea12cb5e3f1ba19ec21522da2b33
ibtkm <ibtkm2009@gmail.com>
2015-04-08 18:34:32 +0430

    wrong format fix

--- app/controllers/recomify_controller.rb
+++ app/controllers/recomify_controller.rb
@@ -7,7 +7,7 @@ class RecomifyController < ApplicationController
   def update
     @current_shop.update_attributes settings: params[:shop][:settings]
     flash[:notice] = "Updated form style"
-    Resque.enqueue(RecommendationSnippetUpdater, @current_shop.id, params[:shop][:settings].merge(server_address: ENV['server_address'])
+    Resque.enqueue(RecommendationSnippetUpdater, @current_shop.id, params[:shop][:settings].merge(server_address: ENV['server_address']))
   	redirect_to settings_recomify_path
   end
 
